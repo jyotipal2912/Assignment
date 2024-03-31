2866a1c8db1e59984467fc4927a4cfe19bab79ed
jyotipal2912 <jyoti29122000@gmail.com>
2024-03-31 23:52:49 +0530

    My Java Assignment

---
+++ Core_Java_Ex_New/corejavaex/Capsule.java
@@ -0,0 +1,20 @@
+package corejavaex;
+
+public class Capsule {
+	public static int nCapsules = 0;
+
+	public double volume;
+
+	public String screenText;
+
+	public Capsule(double volume,String s){
+
+	this.volume = volume;
+
+	screenText = s;
+
+	nCapsules++;
+
+	}
+
+}
