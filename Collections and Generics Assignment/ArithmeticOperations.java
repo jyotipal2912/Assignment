b7c1b7b15cecb9d7af0b87383268c001ced6bb40
jyotipal2912 <jyoti29122000@gmail.com>
2024-04-03 16:46:07 +0530

    Collections & generics

--- Collections and Generics Assignment/ArithmeticOperations.java
+++ Collections and Generics Assignment/ArithmeticOperations.java
@@ -21,8 +21,8 @@ public class ArithmeticOperations {
 	}
 
 	public static <T extends Number> Number sub(List<? extends Number> list) {
-		double d = 0;
-		for (int i = 0; i < list.size(); i++) {
+		double d = list.get(0).doubleValue();
+		for (int i = 1; i < list.size(); i++) {
 			d -= list.get(i).doubleValue();
 		}
 		return new Double(d);
@@ -33,9 +33,21 @@ public class ArithmeticOperations {
 	}
 
 	public static <T extends Number> Number div(List<? extends Number> list) {
+		double d = list.get(0).doubleValue();
+		for (int i = 1; i < list.size(); i++) {
+			d /= list.get(i).doubleValue();
+		}
+		return new Double(d);
+	}
+
+	public static <T extends Number> Number mul(T t1, T t2) {
+		return t1.doubleValue() * t2.doubleValue();
+	}
+
+	public static <T extends Number> Number mul(List<? extends Number> list) {
 		double d = 0;
 		for (int i = 0; i < list.size(); i++) {
-			d /= list.get(i).doubleValue();
+			d *= list.get(i).doubleValue();
 		}
 		return new Double(d);
 	}
@@ -50,20 +62,28 @@ public class ArithmeticOperations {
 	public static void main(String[] args) {
 		// Adding 2 integers
 		Integer i1 = new Integer(34), i2 = new Integer(43);
-		System.out.println("Add with generic method: " + ArithmeticOperations.add(i1, i2));
+		System.out.println("Add Integer with generic method: " + ArithmeticOperations.add(i1, i2));
+		System.out.println("Sub Integer with generic method: " + ArithmeticOperations.sub(i1, i2));
+		System.out.println("Mul Integer with generic method: " + ArithmeticOperations.mul(i1, i2));
+		System.out.println("Div Integer with generic method: " + ArithmeticOperations.div(i1, i2));
 
 		Float f1 = new Float(12.56), f2 = new Float(3.6778);
-		System.out.println("Add with generic method: " + ArithmeticOperations.add(f1, f2));
-		System.out.println("Sub with generic method: " + ArithmeticOperations.sub(f1, f2));
-		System.out.println("Div with generic method: " + ArithmeticOperations.div(f1, f2));
+		System.out.println("Add Float with generic method: " + ArithmeticOperations.add(f1, f2));
+		System.out.println("Sub Float with generic method: " + ArithmeticOperations.sub(f1, f2));
+		System.out.println("Mul Float with generic method: " + ArithmeticOperations.mul(f1, f2));
+		System.out.println("Div Float with generic method: " + ArithmeticOperations.div(f1, f2));
 
 		// Adding 2 integers through a list
 		Vector<Number> l = new Vector<Number>();
 		l.add(new Integer(34));
 		l.add(new Integer(43));
-		System.out.println("Add with upper bounded wildcard: " + ArithmeticOperations.add(l));
+		System.out.println("Add Vector with upper bounded wildcard: " + ArithmeticOperations.add(l));
+		System.out.println("Sub Vector with upper bounded wildcard: " + ArithmeticOperations.sub(l));
+		System.out.println("Mul Vector with upper bounded wildcard: " + ArithmeticOperations.mul(l));
+		System.out.println("Div Vector with upper bounded wildcard: " + ArithmeticOperations.div(l));
 
 		// Dumping the list to the console.
 		ArithmeticOperations.dumpList(l);
 	}
+
 }
